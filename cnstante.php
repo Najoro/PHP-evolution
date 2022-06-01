7f62e4622aa944df89ca0bd6f6406fc559212bc7
Najoro <najofanantenana@gmail.com>
2022-06-01 22:01:28 +0300

    variable, boucle ,condition

---
+++ cnstante.php
@@ -0,0 +1,10 @@
+<?php
+define("CONSTANT", "Oui\n");
+echo CONSTANT;
+echo PHP_INT_MAX . "\n"; // nombre max du variable qu/on peut stacker dans le pc
+echo PHP_INT_MIN . "\n";  // nombre min du variable qu/on peut stacker dans le pc
+echo PHP_INT_SIZE . "\n";
+
+const YS = oui;
+
+echo YS;
---
+++ operateur.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  arithmetique : +,- *,/ , %
+  fontion :
+    intdiv() : return le resulat d'un division en entier;
+    fmod()   : return la modulo 
+
+  affectation : = , += , -=, *= , /=
+  concatenation : .= , .
+  Logique       : and , && , or, ||, xor 
+ */
